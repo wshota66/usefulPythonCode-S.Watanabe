d5f2dc92775d352b93a478b9ce51c121af3bd92a
Shota <51987354+wshota66@users.noreply.github.com>
2023-02-02 13:17:24 -0800

    Update delStringInFilenameMulti.py

--- FileNameOperations/delStringInFilenameMulti.py
+++ FileNameOperations/delStringInFilenameMulti.py
@@ -1,4 +1,13 @@
-# Rename multiple files via GUI
+# Rename multiple files via GUI by deleting a specific exact phrase.
+
+# INSTRUCTIONS:
+# 1. Use Python's pip install to add the necessary libraries: "os" and "tkinter".
+# 2. Run the script through Command Prompt (or Powershell or Terminal)
+# 3. On the Command Prompt, type the exact phrase to delete from the selected file names. Press Enter.
+#    3a. Phrase to delete is case-sensitive.
+# 4. Select file(s) to rename with the prefix from the "Open File" window.
+
+# Note: Each run can only delete the same phrase defined in Step 3. All selected files in Step 4. will have the same exact phrase removed as defined in Step 3.
 
 import os
 from tkinter import filedialog
